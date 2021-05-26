4e5f3f53fa65107972b5115c776fb2b740d3dff3
Matheus <matheusafinovicz@gmail.com>
2021-05-26 15:41:13 -0300

    Book query for existing books

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -2,15 +2,42 @@ import Book from '../models/Book';
 
 class HomeController {
   async index(req, res) {
-    const newBook = await Book.create({
-      name: 'Livro Teste',
-      serie: 'Testando',
-      volume: 2,
-      author: 'Matheus A.',
-      published_at: '2021-05-20',
-      pages: 25,
-    });
-    res.json(newBook);
+    try {
+      const {
+        name = '', serie = '', volume = '', author = '', publishedAt = '', pages = '',
+      } = req.body;
+
+      if (!name || !author || !publishedAt || !pages) {
+        return res.status(400).json({
+          errors: ['Missing required fields'],
+        });
+      }
+
+      const existingBook = await Book.findOne({
+        where: {
+          name, serie, volume,
+        },
+      });
+
+      if (existingBook) {
+        return res.status(400).json({
+          errors: ['This book already exists'],
+        });
+      }
+
+      const newBook = await Book.create({
+        name,
+        serie,
+        volume,
+        author,
+        published_at: publishedAt,
+        pages,
+      });
+
+      return res.json({ sucess: newBook });
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
   }
 }
 
