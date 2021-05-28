17daa3db8d1d47b2ac27cbb82ed7aa05bd1cad4a
Matheus <matheusafinovicz@gmail.com>
2021-05-28 15:04:49 -0300

    Feat: query for one, many or all books on get method

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -3,6 +3,32 @@ import Book from '../models/Book';
 class HomeController {
   async index(req, res) {
     try {
+      const queryParams = { ...req.query };
+
+      if (queryParams) {
+        const validQueries = ['name', 'serie', 'volume', 'author', 'publishedAt', 'pages'];
+        const paramsQueries = Object.keys(queryParams);
+
+        paramsQueries.forEach((key) => {
+          if (!(validQueries.includes(key))) {
+            return res.status(400).json({
+              errors: ['Invalid query params'],
+            });
+          }
+        });
+
+        const books = await Book.findAll({
+          where: queryParams,
+        });
+
+        if (books.length === 0) {
+          return res.status(404).json({
+            errors: ['No books found with the requested params'],
+          });
+        }
+
+        return res.json({ books });
+      }
       const books = await Book.findAll();
 
       return res.json({ books });
